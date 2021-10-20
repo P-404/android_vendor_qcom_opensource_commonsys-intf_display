e165e782b02bf5b4c59eefb42d78e7a4d97d320a
Vishalcj17 <vishalcj@aospa.co>
2021-10-20 18:08:04 +0900

    Revert "vendor-freeze: Move few commonsys-intf/display libs to commonsys/display"
    This reverts commit 7edc05ecad34c42cd16859bc6b5d0bd9513ac745.
    
    Change-Id: Ib2759620f30a185b9b5897f6028795ace7d3b9d2

--- config/display-product-system.mk
+++ config/display-product-system.mk
@@ -1,9 +1,19 @@
-PRODUCT_PACKAGES += vendor.display.config@1.0 \
+PRODUCT_PACKAGES += libdisplayconfig \
+                    libqdMetaData \
+                    libqdMetaData.system \
+                    libgralloc.qti \
+                    libdrm \
+                    vendor.display.config@1.0 \
                     vendor.display.config@1.1 \
                     vendor.display.config@1.2 \
                     vendor.display.config@1.3 \
                     vendor.display.config@1.4 \
                     vendor.display.config@1.5 \
+                    libsmomo.qti \
+                    liblayerext.qti \
+                    libsmomoconfig.qti \
+                    libcomposerextn.qti \
+                    libdisplayconfig.qti \
                     vendor.qti.hardware.display.config-V1-ndk \
                     vendor.qti.hardware.display.config-V2-ndk \
                     vendor.qti.hardware.display.config-V3-ndk \
@@ -14,3 +24,13 @@ PRODUCT_PACKAGES += vendor.display.config@1.0 \
                     vendor.qti.hardware.display.config-V8-ndk \
                     vendor.qti.hardware.display.config-V9-ndk \
                     vendor.qti.hardware.display.config-V10-ndk
+
+SOONG_CONFIG_NAMESPACES += qtidisplaycommonsys
+# Soong Keys
+SOONG_CONFIG_qtidisplaycommonsys := displayconfig_enabled
+# Soong Values
+SOONG_CONFIG_qtidisplaycommonsys_displayconfig_enabled := false
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+    SOONG_CONFIG_qtidisplaycommonsys_displayconfig_enabled := true
+endif
